839f6bcb31c3c9355bf5e1ce6f9fd0f2f26cdb0d
Katie Fritz <katie@katiemfritz.com>
2015-11-17 11:59:25 -0500

    refactor with Chris

--- app.rb
+++ app.rb
@@ -2,10 +2,8 @@ require 'sinatra'
 
 class RandomRedirect < Sinatra::Base
 	get '/' do
-		random = rand(3)
-		redirect("https://katiemfritz.optimalworkshop.com/chalkmark/chnnew-a") if random == 0
-		redirect("https://katiemfritz.optimalworkshop.com/chalkmark/chnnew-b") if random == 1
-		redirect("https://katiemfritz.optimalworkshop.com/chalkmark/chnnew-c") if random == 2
+		base_url = "https://katiemfritz.optimalworkshop.com/chalkmark/chnnew-"
+		redirect base_url + ['a', 'b', 'c'].sample
   end
 
 end
